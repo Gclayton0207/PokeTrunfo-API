09fa665bdd0b06b29f0522889585662cc8ab9591
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-08-08 16:55:09 -0300

    feat: Criando rotas get e post

--- src/controllers/pokemonsController.js
+++ src/controllers/pokemonsController.js
@@ -1,12 +1,27 @@
 import pokemons from "../models/Pokemon.js";
 
-class PokemonController{
+class PokemonController {
+  static home = (req, res) => {
+    res.status(200).send({ mensagem: "Bem vindo ao PokeTrunfo" });
+  };
+  static listarpokemons = (req, res) => {
+    pokemons.find((err, pokemons) => {
+      res.status(200).json(pokemons);
+    });
+  };
+  static cadastrarPokemon = (req, res) => {
+    let pokemon = new pokemons(req.body);
 
-
-    static home = (req, res) => {
-        res.status(200).send({ mensagem: "Bem vindo ao PokeTrunfo" });
+    pokemon.save((err) => {
+      if (err) {
+        res
+          .status(500)
+          .send({ mensagem: `${err.message} - Falha ao cadastrar` });
+      } else {
+        res.status(201).send(pokemon.toJSON());
       }
-
+    });
+  };
 }
 
-export default PokemonController
+export default PokemonController;
--- src/routes/pokemonsRoutes.js
+++ src/routes/pokemonsRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import PokemonController from "../controllers/pokemonsController.js"
+import PokemonController from "../controllers/pokemonsController.js";
 
 const routes = express.Router();
 
-routes.get('/', PokemonController.home);
+routes.get("/", PokemonController.home);
+routes.get("/pokemons", PokemonController.listarpokemons);
+routes.post("/pokemon", PokemonController.cadastrarPokemon);
 
-  export default routes;
+export default routes;
