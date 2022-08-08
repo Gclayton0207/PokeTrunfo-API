01e29ed1ad3b2648cc2070ebc80891f45d5b9fec
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-08-08 17:32:52 -0300

    fix:corrigindo tipos do model

--- src/models/Pokemon.js
+++ src/models/Pokemon.js
@@ -1,20 +1,26 @@
 import mongoose from "mongoose";
 
-const pokemonSchema = new mongoose.Schema({
-  id: { type: "string", required: true },
-  name: { type: "string", required: true },
-  attributes: {
-    type: {
-      hp: { type: Number, required: true },
-      attack: { type: Number, required: true },
-      defense: { type: Number, required: true },
-      "special-attack": { type: Number, required: true },
-      "special-defense": { type: Number, required: true },
-      speed: { type: Number, required: true },
+const pokemonSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    type: { type: [String], required: true },
+    attributes: {
+      type: {
+        hp: { type: Number, required: true },
+        attack: { type: Number, required: true },
+        defense: { type: Number, required: true },
+        spAttack: { type: Number, required: true },
+        spDefense: { type: Number, required: true },
+        speed: { type: Number, required: true },
+        _id: false
+      },
+      required: true,
     },
   },
-});
+  { versionKey: false }
+);
 
-const pokemons = mongoose.model('pokemons', pokemonSchema);
+const pokemons = mongoose.model("pokemons", pokemonSchema);
 
 export default pokemons;
