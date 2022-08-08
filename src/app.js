bb881e7570461e14b739626788bf671ee8ccb791
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-08-08 18:13:34 -0300

    feat: instalando cors e populando banco de dados

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Express from "express";
 import db from "./config/dbConnect.js";
 import bodyParser from "body-parser";
 import routes from "./routes/pokemonsRoutes.js";
+import cors from "cors";
 
 const app = Express();
 
@@ -10,8 +11,10 @@ db.once("open", () => {
   console.log("banco conectado");
 });
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use("/", routes);
+
 
 
 export default app;
