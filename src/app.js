08a02255761ad1e2d4c7459c7c674fc66692e02d
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-08-08 16:07:13 -0300

    fix: alterando banco de dados para mongo e instalando mongoose

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import Express from "express";
+import db from "./config/dbConnect.js";
 import bodyParser from "body-parser";
 
 const app = Express();
+
+db.on("error", console.log.bind(console, "Erro de conexao"));
+db.once("open", () => {
+  console.log("banco conectado");
+});
+
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send({ mensagem:"Bem vindo ao PokeTrunfo"});
+  res.status(200).send({ mensagem: "Bem vindo ao PokeTrunfo" });
 });
-
 
 export default app;
---
+++ src/config/dbConnect.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+  const dbUser = process.env.MONGO_USER;
+  const dbPassword = process.env.MONGO_PASSWORD;
+  const dbCluster = process.env.MONGO_CLUSTER;
+  const dbCollection = process.env.MONGO_COLLECTION;
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}/${dbCollection}`)
+
+let db = mongoose.connection;
+
+export default db;
