b05eaa96469ee4941c7324609c90194eae4d39ab
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-08-08 10:28:47 -0300

    feat: instalando body parser e alterando mensagem de boas vindas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Express from "express";
+import bodyParser from "body-parser";
 
 const app = Express();
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.status(200).send("Bem vindo ao PokeTrunfo");
+  res.status(200).send({ mensagem:"Bem vindo ao PokeTrunfo"});
 });
 
 
